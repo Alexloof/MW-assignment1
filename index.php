46e3093151774397e7a86b8ebac36f760babe4fb
Alex Lööf <alex.loofet@gmail.com>
2019-04-05 11:51:34 +0200

    single unicorn api + display added

--- index.php
+++ index.php
@@ -8,18 +8,18 @@ function debugToConsole($msg)
 
 $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json']]);
 
+$singleUnicorn = (object)[];
+$unicorns = [];
+
 if (isset($_GET["unicorn"])) {
-  debugToConsole($_GET["unicorn"]);
   $uri = 'http://unicorns.idioti.se/' . $_GET["unicorn"];
   $response = $client->request('GET', $uri);
+  $singleUnicorn = json_decode($response->getBody());
 } else {
   $response = $client->request('GET', 'http://unicorns.idioti.se/');
+  $unicorns = json_decode($response->getBody());
 }
-
-
-
-$unicorns = json_decode($response->getBody());
-
+debugToConsole($singleUnicorn);
 ?>
 
 <!doctype html>
@@ -33,30 +33,46 @@ $unicorns = json_decode($response->getBody());
         <div class="container">
           <h1>Enhörningar</h1>
           <hr/>
-          <form action="index.php">
+          <form action="/">
             <div class="form-group">
               <label for="unicorn-id">Id på enhörning</label>
-              <input name="unicorn" type="number" class="form-control" id="unicorn-id">
+              <input name="unicorn" required type="number" class="form-control" id="unicorn-id">
             </div>
             <div class="form-group">
               <button type="submit" class="btn btn-success">Visa enhörning</button>
-              <button type="submit" class="btn btn-primary">Visa alla enhörningar</button>
+              <a  href="/" class="btn btn-primary">Visa alla enhörningar</a>
             </div>
           </form>
           <hr/>
           <?php
-            echo "<h3>Alla enhörningar</h3>";
-            echo "<ul class=\"list-group\">";
-          
+            if (!empty((array) $singleUnicorn)) {
+              echo "<h3>$singleUnicorn->name</h3>";
+              echo "<ul class=\"list-group\">";
+              echo "<div class=\"row\">
+                      <div class=\"col-8\">
+                        <p>$singleUnicorn->spottedWhen</p>
+                        <p>$singleUnicorn->description</p>
+                        <strong>$singleUnicorn->reportedBy</strong>
+                      </div>
+                      <div class=\"col-4\">
+                        <img src=\"$singleUnicorn->image\" alt=\"$singleUnicorn->name\" />
+                      </div>
+                    </div>";
+
+            } else {
+              echo "<h3>Alla enhörningar</h3>";
+              echo "<ul class=\"list-group\">";
             
-            foreach ($unicorns as $unicorn) {
-                echo "<li class=\"list-group-item d-flex justify-content-between align-items-center\">
-                          <p>$unicorn->id $unicorn->name</p>
-                        <span class=\"badge badge-primary badge-pill\">Läs mer</span>
-                      </li>";
+              
+              foreach ($unicorns as $unicorn) {
+                  echo "<li class=\"list-group-item d-flex justify-content-between align-items-center\">
+                            <p>$unicorn->id $unicorn->name</p>
+                          <a href=\"/?unicorn=$unicorn->id\" class=\"badge badge-primary badge-pill\" style=\"cursor: pointer; color: white\">Läs mer</a>
+                        </li>";
+              }
+              
+              echo "</ul>";
             }
-            
-            echo "</ul>";
           ?>
         </div>
     </body>
