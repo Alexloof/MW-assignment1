9dc2ac59e49a6a77ef5528219d61907fffb32b64
Alex Lööf <alex.loofet@gmail.com>
2019-04-05 12:22:56 +0200

    added error handling  + logging to file

--- index.php
+++ index.php
@@ -1,25 +1,38 @@
 <?php
 require 'vendor/autoload.php';
 
-function debugToConsole($msg)
-{
-    echo "<script>console.log(" . json_encode($msg) . ")</script>";
-}
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('Assignment 1');
+$log->pushHandler(new StreamHandler('logs.log', Logger::INFO));
 
 $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json']]);
-
 $singleUnicorn = (object)[];
 $unicorns = [];
+$errorMessage = '';
 
 if (isset($_GET["unicorn"])) {
   $uri = 'http://unicorns.idioti.se/' . $_GET["unicorn"];
-  $response = $client->request('GET', $uri);
-  $singleUnicorn = json_decode($response->getBody());
+  try {
+    $response = $client->request('GET', $uri);
+    $singleUnicorn = json_decode($response->getBody());
+    $log->info('Requested info about' . $singleUnicorn->name);
+  } catch(Exception $e) {
+    $log->error($e->getMessage());
+    $errorMessage = "We could not find the unicorn you were searching for...";
+  }
+  
 } else {
-  $response = $client->request('GET', 'http://unicorns.idioti.se/');
-  $unicorns = json_decode($response->getBody());
+  try {
+    $response = $client->request('GET', 'http://unicorns.idioti.se/');
+    $unicorns = json_decode($response->getBody());
+    $log->info('Requested info about alla unicorns');
+  } catch (Exception $e) {
+    $log->error($e->getMessage());
+    $errorMessage = "We could not load the unicorns...";
+  }
 }
-debugToConsole($singleUnicorn);
 ?>
 
 <!doctype html>
@@ -45,12 +58,19 @@ debugToConsole($singleUnicorn);
           </form>
           <hr/>
           <?php
+            if ($errorMessage) {
+              echo "<h4>$errorMessage<h4/>";
+              exit();
+            }
+
             if (!empty((array) $singleUnicorn)) {
+              $formattedDate = date_format(date_create($singleUnicorn->spottedWhen),"Y/m/d");
+
               echo "<h3>$singleUnicorn->name</h3>";
               echo "<ul class=\"list-group\">";
               echo "<div class=\"row\">
                       <div class=\"col-8\">
-                        <p>$singleUnicorn->spottedWhen</p>
+                        <p>$formattedDate</p>
                         <p>$singleUnicorn->description</p>
                         <strong>$singleUnicorn->reportedBy</strong>
                       </div>
@@ -58,19 +78,15 @@ debugToConsole($singleUnicorn);
                         <img src=\"$singleUnicorn->image\" alt=\"$singleUnicorn->name\" />
                       </div>
                     </div>";
-
             } else {
               echo "<h3>Alla enhörningar</h3>";
               echo "<ul class=\"list-group\">";
-            
-              
               foreach ($unicorns as $unicorn) {
                   echo "<li class=\"list-group-item d-flex justify-content-between align-items-center\">
                             <p>$unicorn->id $unicorn->name</p>
                           <a href=\"/?unicorn=$unicorn->id\" class=\"badge badge-primary badge-pill\" style=\"cursor: pointer; color: white\">Läs mer</a>
                         </li>";
               }
-              
               echo "</ul>";
             }
           ?>
